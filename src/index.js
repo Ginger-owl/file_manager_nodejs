57de6543e4e21b726b305d8c4ab2718e73ef6336
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-30 18:48:08 +0400

    fix: fix isuue with username and unexpected exiting

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ export default class FileManager {
     const additionalArgs = process.argv.slice(2)
     if (additionalArgs.length > 0) {
       additionalArgs.forEach((value, index) => {
-        if (value === '--username') {
-          username = additionalArgs[index + 1]
+        if (value.startsWith('--username')) {
+          username = value.split('=')[1]
         }
       })
     return username
@@ -35,75 +35,75 @@ export default class FileManager {
       case 'up':
         if (params.length === 0) {
           this.currentDirectory = up(this.currentDirectory)
-          break
         }
+        break
       case 'ls':
         if (params.length === 0) {
           await ls(this.currentDirectory)
-          break
         }
+        break
       case 'cd':
         if (params.length === 1) {
           this.currentDirectory = await cd(params[0], this.currentDirectory)
-          break
         }
+        break
       case 'cat':
         if (params.length === 1) {
           await cat(params[0], this.currentDirectory)
-          break
         } else {
           console.log('Invalid input!') //TODO: refactor
         }
+        break
       case 'cp':
         if (params.length === 2) {
           await cp(params[0], params[1], this.currentDirectory)
-          break
         } else {
           console.log('Invalid input!') //TODO: refactor
         }
+        break
       case 'mv':
         if (params.length === 2) {
           await mv(params[0], params[1], this.currentDirectory)
-          break
         } else {
           console.log('Invalid input!') //TODO: refactor
         }
+        break
       case 'rm':
         if (params.length === 1) {
           await rm(params[0], this.currentDirectory)
-          break
         } else {
           console.log('Invalid input!') //TODO: refactor
         }
+        break
       case 'rn':
         if (params.length === 2) {
           await rn(params[0], params[1], this.currentDirectory)
-          break
         } else {
           console.log('Invalid input!') //TODO: refactor
         }
+        break
       case 'add':
         if (params.length === 1) {
           await add(params[0], this.currentDirectory)
-          break
         } else {
           console.log('Invalid input!') //TODO: refactor
         }
+        break
       case 'hash':
         if (params.length === 1) {
           await calculateHash(params[0], this.currentDirectory)
-          break
         }
+        break
       case 'compress':
         if (params.length === 2) {
           await compress(params[0], params[1], this.currentDirectory)
-          break
         }
+        break
       case 'decompress':
         if (params.length === 2) {
           await decompress(params[0], params[1], this.currentDirectory)
-          break
         }
+        break
       case '.exit':
         console.log(`Thank you for using File Manager, ${this.username}, goodbye!`)
         process.exit(0);
@@ -111,7 +111,7 @@ export default class FileManager {
         console.log(`Invalid input!`)
         break
     }
-    process.stdout.write(`You are currently in ${this.currentDirectory}`)
+    console.log(`You are currently in ${this.currentDirectory}`)
   }
 
   init() {
